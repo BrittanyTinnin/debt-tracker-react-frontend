16f940baabc141110a49e830e8db8816031d6ea5
github username <github email address>
2019-03-30 01:44:27 -0400

    add functions to userActions

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,3 +1,15 @@
-export const signUp = () => dispatch => {
+export const signUp = (user) => dispatch => {
+
+}
+
+export const authenticate = () => dispatch => {
+
+}
+
+export const login = () => dispatch => {
+
+}
+
+export const logout = () => dispatch => {
 
 }
